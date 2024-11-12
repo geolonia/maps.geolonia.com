9c0d22a6ebe29d4be5cf65cf8a1c5c563b512d2d
himagone <sunfgirl1975@icloud.com>
2024-11-12 12:38:52 +0900

    イマイチ

--- src/FeatureInfo.ts
+++ src/FeatureInfo.ts
@@ -1,4 +1,24 @@
 import maplibregl from 'maplibre-gl';
+// タグの日本語変換マッピング
+const tagTranslations: { [key: string]: string } = {
+  'addr:block_number': '街区番号',
+  'addr:city': '市区町村',
+  'addr:housenumber': '番地',
+  'addr:neighbourhood': '丁目',
+  'addr:postcode': '郵便番号',
+  'addr:province': '都道府県',
+  'addr:quarter': '地区',
+  'brand': 'ブランド',
+  'brand:en': 'ブランド(英語)',
+  'brand:ja': 'ブランド(日本語)',
+  'brand:wikidata': 'Wikidata ID',
+  'brand:wikipedia': 'Wikipedia',
+  'name': '名称',
+  'name:en': '名称(英語)',
+  'name:ja': '名称(日本語)',
+  'opening_hours': '営業時間',
+  'shop': '店舗種別'
+};
 
 const addFeatureInfoHandler = (map: maplibregl.Map) => {
   map.on('click', async (e) => {
@@ -21,15 +41,20 @@ const addFeatureInfoHandler = (map: maplibregl.Map) => {
       });
       
       const data = await response.json();
-      console.log('Feature info:', data);
       if (data.elements && data.elements.length > 0) {
         const feature = data.elements[0];
         const tags = feature.tags || {};
+        //各要素を取得
         // 表示名を取得
         const name = tags.name || tags['name:ja'] || tags['name:en'] || tags['name:zh'];
+        const wikidata = tags['brand:wikidata'];
+        const source_ref = tags['source_ref'];
         // ポップアップを作成
         const popupContent = Object.entries(tags)
-          .map(([key, value]) => `<div><strong>${key}:</strong> ${value}</div>`)
+          .map(([key, value]) => {
+            const translatedKey = tagTranslations[key] || key;
+            return `<div><strong>${translatedKey}:</strong> ${value}</div>`;
+          })
           .join('');
         new maplibregl.Popup()
           .setLngLat([lng, lat])
@@ -37,6 +62,9 @@ const addFeatureInfoHandler = (map: maplibregl.Map) => {
             <div style="max-height: 300px; overflow-y: auto;">
               <h3 style="margin-bottom: 8px; font-weight: bold;">${name || ''}</h3>
               ${popupContent || 'この地点には情報がありません'}
+              ${wikidata ? `<div><a href="https://www.wikidata.org/wiki/${wikidata}" target="_blank">Wikidata</a>を見に行く</div>` : ''}
+              ${name ? `<div><a href="https://ja.wikipedia.org/wiki/${name}" target="_blank">Wikipedia</a>を見に行く</div>` : ''}
+              ${source_ref ? `<div><a href="${source_ref}" target="_blank">参照先</a>を見に行く</div>` : ''}
             </div>
           `)
           .addTo(map);
